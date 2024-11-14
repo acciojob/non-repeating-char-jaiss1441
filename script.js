1d086a64931167b2dbe8ac213a25ce3db35b8029
Jaiswal Vikas <jaiswalvikas7276@gmail.com>
2024-11-14 11:20:53 -0800

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function firstNonRepeatedChar(str) {
+	
  // Write your code here
+	let obj={}
+
+	for(let char of str){
+		obj[chr] = (obj[char] ||0)+1;
+	}
+
+	for(let char of str){
+
+	if(obj[char]===1){
+		return char;
+	}
+	}
+	return null;
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+// const input = prompt("Enter a string");
+// alert(firstNonRepeatedChar(input)); 
